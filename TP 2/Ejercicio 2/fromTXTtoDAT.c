8f9f3634fefb5e9b0359d90b052f50b5f2f225ad
carluandraster <andres.efstratiadis@gmail.com>
2025-09-06 12:25:27 -0300

    Update fromTXTtoDAT.c

--- TP 2/Ejercicio 2/fromTXTtoDAT.c
+++ TP 2/Ejercicio 2/fromTXTtoDAT.c
@@ -1,1 +1,27 @@
 #include <stdio.h>
+
+typedef struct
+{
+    char codigoUniversidad[5];
+    char codigoFacultad[5];
+    char codigoAsignatura[5];
+    int legajo;
+    int edad;
+    char codigoDocente[5];
+    float nota;
+    char observaciones[255];
+    char baja; // 1 si esta dado de baja, 0 si no
+} AlumnoFijo;
+
+typedef struct
+{
+    char* codigoUniversidad;
+    char* codigoFacultad;
+    char* codigoAsignatura;
+    int legajo;
+    int edad;
+    char* codigoDocente;
+    float nota;
+    char* observaciones;
+    char baja; // 1 si esta dado de baja, 0 si no
+} AlumnoVariable;
