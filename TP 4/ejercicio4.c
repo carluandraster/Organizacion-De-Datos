d4414bfd0d14c76cb61e193fd085003f0e1953ce
carluandraster <andres.efstratiadis@gmail.com>
2025-09-21 11:52:32 -0300

    Ejecutar y compilar ejercicio 4

--- TP 4/ejercicio4.c
+++ TP 4/ejercicio4.c
@@ -2,30 +2,60 @@
 #include <math.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 
-int redondear(double x){
-    double parteFraccionaria = x-round(x);
-    if(parteFraccionaria < 0.5)
+/**
+ * Redondea un numero decimal al entero mas cercano
+ * Si la parte fraccionaria es igual o mayor a 0.5, redondea al entero mayor; de lo contrario, al menor
+ *
+ * Parametros:
+ *      - x (double) - numero decimal a redondear
+ *
+ * Retorna: (int) - numero redondeado
+ */
+int redondear(double x)
+{
+    double parteFraccionaria = x - round(x);
+    if (parteFraccionaria < 0.5)
         return round(x);
     else
-        return round(x+1);
+        return round(x + 1);
 }
 
-int booleanoAleatorio(){
-    return redondear((double) rand()/RAND_MAX);
+// Devuelve 0 o 1 de forma aleatoria
+// No requiere parametros
+// Retorna: 0 con probabilidad 0.5 y 1 con probabilidad 0.5
+int booleanoAleatorio()
+{
+    return redondear((double)rand() / RAND_MAX);
 }
 
-void insertarRandom(ArbolBinario* arbol){
+void insertarRandom(ArbolBinario *arbol)
+{
     int dato = rand();
-    if(arbol == NULL)
+    int izquierda;
+    if (arbol == NULL)
         *arbol = crearNodo(dato);
-    else{
-        int izquierda = booleanoAleatorio();
+    else
+    {
+        izquierda = booleanoAleatorio();
+        if (izquierda)
+            insertarRandom(getSubarbolIzq(*arbol));
+        else
+            insertarRandom(getSubarbolDer(*arbol));
     }
 }
 
 int main(int argc, char const *argv[])
 {
-    
+    srand(time(NULL)); // Inicializa la semilla para la generacion de numeros aleatorios
+    ArbolBinario arbol = NULL;
+
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d\n", rand());
+        insertarRandom(&arbol);
+    }
+
     return 0;
 }
---
+++ TP 4/integracion.bat
@@ -0,0 +1,4 @@
+@echo off
+cd "TP 4"
+gcc ArbolBinario.c ejercicio4.c -o ejercicio4.exe
+exit
