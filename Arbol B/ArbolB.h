d96c1c650800cedb46cc86e1130ba156ad301622
carluandraster <andres.efstratiadis@gmail.com>
2025-10-04 15:33:36 -0300

    Create ArbolB.h

---
+++ Arbol B/ArbolB.h
@@ -0,0 +1,20 @@
+#include "ListaOrdenada.h"
+
+typedef TElementoLista TElementoArbol;
+typedef struct NodoArbol
+{
+    ListaOrdenada *claves;
+    struct NodoArbol **hijos; // Vector de punteros a hijos
+} NodoArbol;
+typedef struct
+{
+    NodoArbol *raiz;
+    int orden; // Orden del árbol B
+} ArbolB;
+
+ArbolB *crearArbolB(int orden);
+void insertar(ArbolB *arbol, TElementoArbol clave);
+void _insertar(ArbolB *arbol, TElementoArbol clave, TElementoArbol *promovida);
+void eliminar(ArbolB *arbol, TElementoArbol clave);
+TElementoArbol *recorrer(ArbolB *arbol);
+int len(ArbolB *arbol);
