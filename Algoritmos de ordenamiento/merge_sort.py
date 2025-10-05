6a4737a1e30a9c63a1d759f8631cda0dd0c9f55e
carluandraster <andres.efstratiadis@gmail.com>
2025-10-05 14:30:27 -0300

    Create merge_sort.py

---
+++ Algoritmos de ordenamiento/merge_sort.py
@@ -0,0 +1,12 @@
+def merge_sort(arr: list) -> list:
+    """"
+    Recibe una lista desordenada y la ordena por el método Merge Sort
+    Pre: arr es distinto de None y tiene al menos un elemento
+    
+    Parámetros:
+        - arr (list): lista desordenada
+        
+    Retorna: lista ordenada
+    """
+    auxiliar1, auxiliar2: list
+    
