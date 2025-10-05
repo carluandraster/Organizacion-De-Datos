cb8deb660ca700855dfc73c2781606d136b093a9
carluandraster <andres.efstratiadis@gmail.com>
2025-10-05 15:28:55 -0300

    Update merge_sort.py

--- Algoritmos de ordenamiento/merge_sort.py
+++ Algoritmos de ordenamiento/merge_sort.py
@@ -8,11 +8,11 @@ def merge_sort(origen: list) -> list:
         
     Retorna: lista ordenada
     """
-    auxiliar1, auxiliar2 = [], []
     insertarEnAuxiliar1 = True
     esta_ordenado = False
     
     while not esta_ordenado:
+        auxiliar1, auxiliar2 = [], []
         esta_ordenado = True # La lista está ordenada hasta que se demuestre lo contrario
         auxiliar1.append(origen[0])
         for i in range(1, len(origen)):
@@ -30,7 +30,7 @@ def merge_sort(origen: list) -> list:
             i = 0
             j = 0
             while i < len(auxiliar1) or j < len(auxiliar2):
-                while i < len(auxiliar1) or j < len(auxiliar2) or i == 0 or auxiliar1[i] > auxiliar1[i-1] or j == 0 or auxiliar2[j] > auxiliar2[j-1]:
+                while (i < len(auxiliar1) or j < len(auxiliar2)) and (primera_subiteracion1 or auxiliar1[i] > auxiliar1[i-1]) and (primera_subiteracion2 or auxiliar2[j] > auxiliar2[j-1]):
                     if (primera_subiteracion1 or auxiliar1[i] > auxiliar1[i-1]) and (primera_subiteracion2 or auxiliar2[j] > auxiliar2[j-1]):
                         if auxiliar1[i] < auxiliar2[j]:
                             origen.append(auxiliar1[i])
