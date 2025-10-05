2a255f36c3aeac820352d6696add40d27515f4dc
carluandraster <andres.efstratiadis@gmail.com>
2025-10-05 14:34:51 -0300

    Update merge_sort.py

--- Algoritmos de ordenamiento/merge_sort.py
+++ Algoritmos de ordenamiento/merge_sort.py
@@ -9,4 +9,18 @@ def merge_sort(arr: list) -> list:
     Retorna: lista ordenada
     """
     auxiliar1, auxiliar2: list
+    insertarEnAuxiliar1 = True
+    esta_ordenado = False
     
+    while not esta_ordenado:
+        vc = arr[0]
+        esta_ordenado = True
+        for i in range(1, len(arr)):
+            if arr[i] < vc:
+                vc = arr[i]
+                insertarEnAuxiliar1 = not insertarEnAuxiliar1
+                esta_ordenado = False
+            if insertarEnAuxiliar1:
+                auxiliar1.append(arr[i])
+            else:
+                auxiliar2.append(arr[i])
