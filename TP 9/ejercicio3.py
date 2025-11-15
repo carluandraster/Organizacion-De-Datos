f2881df36317b954b68ce89c43c1a562ab7de16f
carluandraster <andres.efstratiadis@gmail.com>
2025-11-15 19:28:22 -0300

    Update ejercicio3.py

--- TP 9/ejercicio3.py
+++ TP 9/ejercicio3.py
@@ -1,8 +1,10 @@
-from vigenere import encriptar_con_vigenere
+from vigenere import encriptar_con_vigenere, ALFABETO
 from numpy import array
 
 FRASE = "El cifrado Cesar, es una de las tecnicas mas simples; consiste en reemplazar cada letra por otra que se encuentra un numero fijo de posiciones mas adelante."
+FRASE_POLIBIO = "El cifrado César, es una de las técnicas más simples; consiste en reemplazar cada letra por otra que se encuentra un número fijo de posiciones más adelante."
 CLAVE = "Ventilado"
+CLAVE_CONFEDERADOS = "julio"
 CUADRADO_DE_POLIBIO = array([
     'a','b','c','d','e','f','g','h',
     'i','j','k','l','m','n','o','p',
@@ -24,7 +26,20 @@ def encriptar_con_polibio(frase: str) -> str:
         else:
             frase_encriptada += char
     return frase_encriptada.strip()
-    
+
+def metodo_de_confederados(frase: str, clave: str) -> str:
+    i = 0
+    frase_encriptada = ""
+    for char in frase:
+        if char in ALFABETO:
+            indice_frase = ALFABETO.index(char)
+            indice_clave = ALFABETO.index(clave[i % len(clave)])
+            nuevo_indice = (indice_frase + indice_clave) % len(ALFABETO)
+            frase_encriptada += ALFABETO[nuevo_indice]
+            i += 1
+        else:
+            frase_encriptada += char
+    return frase_encriptada
 
 print("Frase original:")
 print(FRASE)
@@ -33,5 +48,9 @@ frase_encriptada = encriptar_con_vigenere(FRASE, CLAVE)
 print(frase_encriptada)
 
 print("\nFrase encriptada con Polibio:")
-frase_encriptada_polibio = encriptar_con_polibio(FRASE)
+frase_encriptada_polibio = encriptar_con_polibio(FRASE_POLIBIO)
 print(frase_encriptada_polibio)
+
+print("\nFrase encriptada con el Metodo de los Confederados:")
+frase_encriptada_confederados = metodo_de_confederados(FRASE, CLAVE_CONFEDERADOS)
+print(frase_encriptada_confederados)
